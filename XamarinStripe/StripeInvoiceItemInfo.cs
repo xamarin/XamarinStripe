5e071d46840d966076f65afa5fd58341b32dfeef
Billy Coover <billycoover@gmail.com>
2013-09-20 23:24:38 -0700

    remove extra param

--- XamarinStripe/StripeInvoiceItemInfo.cs
+++ XamarinStripe/StripeInvoiceItemInfo.cs
@@ -35,7 +35,7 @@ namespace Xamarin.Payments.Stripe {
         #region IUrlEncoderInfo implementation
         public virtual void UrlEncode (StringBuilder sb)
         {
-            sb.AppendFormat ("customer={0}&amount={1}&currency={2}&",
+            sb.AppendFormat ("customer={0}&amount={1}&currency={2}",
                 HttpUtility.UrlEncode (CustomerID), Amount, HttpUtility.UrlEncode (Currency ?? "usd"));
             if (!string.IsNullOrEmpty (Description))
                 sb.AppendFormat ("&description={0}", HttpUtility.UrlEncode (Description));
